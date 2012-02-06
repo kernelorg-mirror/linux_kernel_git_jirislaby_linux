e36eb3bdc495e0350d5a53b6335bd65de6b08fc1
Jiri Slaby <jirislaby@gmail.com>
2012-02-06 12:00:02 +0100

    AI: pass lock pointer to ai_load/store
    Signed-off-by: Jiri Slaby <jirislaby@gmail.com>

--- include/linux/ai.h
+++ include/linux/ai.h
@@ -13,17 +13,17 @@ extern void __assert_fail (__const char *__assertion, __const char *__file,
       ? (void)(0)							\
       : __assert_fail(#expr, __FILE__, __LINE__, __PRETTY_FUNCTION__))
 
-extern volatile int __ai_load(const char *what);
-extern void __ai_store(const char *what, int val);
+extern volatile int __ai_load(void *lock, const char *what);
+extern void __ai_store(void *lock, const char *what, int val);
 
 #define __ai_lock(L) do {				\
-	__ai_assert(__ai_load(__stringify(L)) == 0);	\
-	__ai_store(__stringify(L), 1);			\
+	__ai_assert(__ai_load(L, __stringify(L)) == 0);	\
+	__ai_store(L, __stringify(L), 1);		\
 } while (0)
 
 #define __ai_unlock(L) do {				\
-	__ai_assert(__ai_load(__stringify(L)) == 1);	\
-	__ai_store(__stringify(L), 0);			\
+	__ai_assert(__ai_load(L, __stringify(L)) == 1);	\
+	__ai_store(L, __stringify(L), 0);		\
 } while (0)
 
 #endif
