e4ba7c70b3351f6fee76be1f1c7f863646d2d3ec
Jiri Slaby <jirislaby@gmail.com>
2012-09-04 14:32:58 +0200

    ai: add support for ptreadchecker
    Signed-off-by: Jiri Slaby <jirislaby@gmail.com>

--- include/linux/ai.h
+++ include/linux/ai.h
@@ -17,10 +17,14 @@ extern void __assert_fail (__const char *__assertion, __const char *__file,
 
 extern volatile int __ai_load(void *lock, const char *what);
 extern void __ai_store(void *lock, const char *what, int val);
+/* int required for clang checker */
+static inline int __ai_lock(void *lock) { return 1; }
+static inline void __ai_unlock(void *lock) { }
 
 #define __ai_lock(L) do {				\
 	__ai_assert(__ai_load(L, __stringify(L)) == 0);	\
 	__ai_store(L, __stringify(L), 1);		\
+	__ai_lock(L);					\
 } while (0)
 
 #define __ai_lock_cond(L) ({			\
@@ -33,6 +37,7 @@ extern void __ai_store(void *lock, const char *what, int val);
 #define __ai_unlock(L) do {				\
 	__ai_assert(__ai_load(L, __stringify(L)) == 1);	\
 	__ai_store(L, __stringify(L), 0);		\
+	__ai_unlock(L);					\
 } while (0)
 
 #endif
